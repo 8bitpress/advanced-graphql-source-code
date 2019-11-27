f6c1e9510d0752b0e2b431b79ba7aa8b69b9b194
Mandi Wise <hello@mandiwise.com>
2019-11-27 07:56:15 -0800

    Set up basic React app.

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+const App = () => (
+  <div>
+    <p>Hello, world!</p>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
