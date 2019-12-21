e133a3036e845fc55b7489fc9dbe436c7027c1f8
Mandi Wise <hello@mandiwise.com>
2019-12-21 08:32:14 -0700

    Add error handling to Apollo Link.

--- client/src/graphql/apollo.js
+++ client/src/graphql/apollo.js
@@ -1,5 +1,6 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloLink, InMemoryCache } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
+import { onError } from "apollo-link-error";
 
 import typePolicies from "./typePolicies";
 
@@ -7,11 +8,26 @@ const client = new ApolloClient({
   cache: new InMemoryCache({
     typePolicies
   }),
-  link: createUploadLink({
-    credentials:
-      process.env.NODE_ENV === "production" ? "same-origin" : "include",
-    uri: process.env.REACT_APP_GRAPHQL_ENDPOINT
-  })
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        graphQLErrors.forEach(
+          ({ extensions: { serviceName }, message, path }) =>
+            console.log(
+              `[GraphQL error]: Message: ${message}, Service: ${serviceName}, Path: ${path[0]}`
+            )
+        );
+      }
+      if (networkError) {
+        console.log(`[Network error]: ${networkError}`);
+      }
+    }),
+    createUploadLink({
+      credentials:
+        process.env.NODE_ENV === "production" ? "same-origin" : "include",
+      uri: process.env.REACT_APP_GRAPHQL_ENDPOINT
+    })
+  ])
 });
 
 export default client;
