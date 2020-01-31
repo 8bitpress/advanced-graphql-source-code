7b058075ad2ce316ba0375c3a6510792d18d90eb
Mandi Wise <hello@mandiwise.com>
2020-01-30 16:32:20 -0800

    Update Certbot URLs.

--- scripts/init-letsencrypt.sh
+++ scripts/init-letsencrypt.sh
@@ -9,7 +9,7 @@
 
 domain=${1}
 rsa_key_size=4096
-data_path="./certbot"
+data_path="/home/chirpsdev/devchirps/certbot"
 email=${2:-""}
 staging=${3:-0}
 
@@ -23,8 +23,8 @@ fi
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
   echo "### Downloading recommended TLS parameters ..."
   mkdir -p "$data_path/conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
   echo
 fi
 
